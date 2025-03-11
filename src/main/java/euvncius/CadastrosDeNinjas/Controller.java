e00eec604b4ec6dc06d230c7435cfa9c9f586f7e
euvncius <viniciuscandidocontato@hotmail.com>
2025-03-11 15:09:29 -0300

    Criado a controller

---
+++ src/main/java/euvncius/CadastrosDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package euvncius.CadastrosDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //mesmo do rest API
+@RequestMapping //precisa sempre, para fazer o mapeamento do site
+public class Controller {
+
+    @GetMapping("/boasvindas") //boas praticas colocar um barra antes
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+}
